00ea9b39415babf75e3631deb6ece9c7e6c3c1b4
Kiran-Shehzadi-dev <shehzadik611@gmail.com>
2026-08-10 07:18:40 +0500

    components

---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='nav'><h1>This is a navigation menu.</h1></div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,5 @@
+function Card(){
+    return <div className="Card"><h1>Hi, I am Kiran Shehzadi</h1><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A aperiam architecto molestias tempora aut beatae labore eius. Ea, consequatur atque. Enim deleniti sapiente quae similique repellendus veniam minus repudiandae voluptas.</p></div>
+
+}
+export default Card;
