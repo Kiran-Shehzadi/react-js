b3ee88d1d75a738427b07a855538407ea2dd4381
Kiran-Shehzadi-dev <shehzadik611@gmail.com>
2026-08-10 07:22:57 +0500

    functions

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  function btn(){
+    console.log("heelllooo");
+    
+  }
+  return (
+    <div>
+      <button>CLick</button>
+    </div>
+  )
+}
+
+export default App
